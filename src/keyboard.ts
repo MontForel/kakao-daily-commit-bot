c2dd44ca0f67d6d090f96c7a276c80d652896519
Kevin Kwon <mont.forel@gmail.com>
2018-03-14 23:45:08 +0900

    Add logwatching when event is received

--- src/keyboard.ts
+++ src/keyboard.ts
@@ -6,6 +6,8 @@ interface IResponseBody {
 }
 
 export const main: Handler = (event: any, context: Context, callback: Callback) => {
+  console.log(`메시지가 수신되었습니다: ${JSON.stringify(event)}`)
+  
   const response: IResponseBody = {
     statusCode: 200,
     body: JSON.stringify({
